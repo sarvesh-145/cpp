cd3b8e5ca2145a7fb3a9a2f6439906acfa4ac5bb
sarvesh-145 <sarveshhwaran14@gmail.com>
2026-08-01 13:09:20 +0530

    Check if the entered number is odd or even

---
+++ check.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the number: ";
+    cin>>n;
+    if(n==0&&n%2==0)
+    {
+        cout<<"the number is even"<<endl;
+    }
+    else
+    {
+        cout<<"the number is odd"<<endl;
+    }
+    return 0;
+}
